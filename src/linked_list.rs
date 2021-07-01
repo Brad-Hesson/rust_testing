2c4d556f1e9a6735cdb0b50629d6c5dc543d7aba
Brad Hesson <brad.hesson@outlook.com>
2021-07-01 12:04:24 -0600

    implemented singly linked lists

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,18 +1,30 @@
+#[derive(Debug)]
+pub struct Node<T> {
+    elem: T,
+    link: Option<Box<Node<T>>>
+}
+
+#[derive(Debug)]
 pub struct List<T> {
-    object: Option<T>,
-    next: Option<Box<List<T>>>
+    head: Option<Box<Node<T>>>
 }
 
 impl<T> List<T>{
-    pub fn new() -> Self{
-        Self {
-            object: Option::None,
-            next: Option::None
-        }
+    pub fn new() -> Self {
+        Self { head: Option::None }
     }
 
     pub fn push(&mut self, item: T) {
-        self.object = Some(item);
+        self.head = Some(Box::new(Node{elem: item, link: self.head.take()}));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if let Some(node) = self.head.take(){
+            self.head = node.link;
+            Some(node.elem)
+        } else {
+            None
+        }
     }
 }
 
@@ -21,9 +33,14 @@ impl<T> List<T>{
 mod tests {
     use super::*;
     #[test]
-    fn test1() {
+    fn list_test() {
         let mut l = List::<i32>::new();
+        l.push(1);
+        l.push(2);
         l.push(3);
-        println!("{:?}", l.object);
+        assert_eq!(l.pop(), Some(3));
+        assert_eq!(l.pop(), Some(2));
+        assert_eq!(l.pop(), Some(1));
+        assert_eq!(l.pop(), None);
     }
 }
