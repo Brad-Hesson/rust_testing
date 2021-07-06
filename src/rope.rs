eee1eb8230d5feb5ad426b527b2852880335ed03
Brad Hesson <brad.hesson@outlook.com>
2021-07-05 21:16:33 -0600

    big changes and cleanup

--- src/rope.rs
+++ src/rope.rs
@@ -1,48 +1,62 @@
 use std::{fmt::Debug, str, usize};
 
 pub struct Rope {
-    inner: RopeInner,
+    inner: Box<RopeInner>,
 }
 impl Debug for Rope {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.inner.fmt(f)
     }
 }
-
-impl Rope {
-    pub fn new() -> Self {
+impl From<RopeInner> for Rope {
+    fn from(ri: RopeInner) -> Self {
         Self {
-            inner: RopeInner::new(),
+            inner: Box::new(ri),
         }
     }
+}
+impl From<String> for Rope {
+    fn from(string: String) -> Self {
+        Self::from(RopeInner::Leaf(Leaf::from(string)))
+    }
+}
+impl From<&str> for Rope {
+    fn from(string: &str) -> Self {
+        Self::from(string.to_string())
+    }
+}
+impl Rope {
+    pub fn new() -> Self {
+        Self::from(RopeInner::new())
+    }
     pub fn len(&self) -> usize {
-        self.inner.get_weight()
+        self.inner.len()
     }
     pub fn get_string(&self) -> String {
         self.inner.get_string()
     }
     pub fn concat(left: Self, right: Self) -> Self {
-        Self {
-            inner: RopeInner::concat(left.inner, right.inner),
-        }
+        Self::from(RopeInner::concat(*left.inner, *right.inner))
     }
-
     pub fn split(self, i: usize) -> (Self, Self) {
         let (left, right) = self.inner.split(i);
-        (Self { inner: left }, Self { inner: right })
+        (Self::from(left), Self::from(right))
     }
     pub fn insert(&mut self, rope: Self, i: usize) {
         let old = std::mem::replace(self, Self::new());
         let (left, right) = old.split(i);
         let new = Self::concat(Self::concat(left, rope), right);
-        let _ = std::mem::replace(self, new);
+        drop(std::mem::replace(self, new));
     }
     pub fn delete(&mut self, i: usize, j: usize) {
         let old = std::mem::replace(self, Self::new());
         let (left, rest) = old.split(i);
         let (_, right) = rest.split(j - i);
         let new = Self::concat(left, right);
-        let _ = std::mem::replace(self, new);
+        drop(std::mem::replace(self, new));
+    }
+    pub fn index(&self, index: usize) -> String {
+        self.inner.index(index)
     }
 }
 
@@ -63,27 +77,6 @@ impl Debug for RopeInner {
         }
     }
 }
-impl From<String> for Rope {
-    fn from(string: String) -> Self {
-        Self {
-            inner: RopeInner::Leaf(Leaf {
-                weight: string.len(),
-                string,
-            }),
-        }
-    }
-}
-impl From<&str> for Rope {
-    fn from(string: &str) -> Self {
-        Self {
-            inner: RopeInner::Leaf(Leaf {
-                weight: string.len(),
-                string: string.to_string(),
-            }),
-        }
-    }
-}
-
 impl RopeInner {
     fn new() -> Self {
         Self::Node(Node {
@@ -92,10 +85,17 @@ impl RopeInner {
             weight: 0,
         })
     }
-    fn get_weight(&self) -> usize {
+    fn len(&self) -> usize {
         match self {
-            Self::Node(node) => node.weight,
             Self::Leaf(leaf) => leaf.weight,
+            Self::Node(node) => {
+                node.weight
+                    + node
+                        .right
+                        .as_ref()
+                        .and_then(|r| Some(r.len()))
+                        .unwrap_or_default()
+            }
         }
     }
     fn get_string(&self) -> String {
@@ -119,14 +119,8 @@ impl RopeInner {
         }
     }
     fn concat(left: Self, right: Self) -> Self {
-        if left.get_weight() == 0 {
-            return right;
-        }
-        if right.get_weight() == 0 {
-            return left;
-        }
         Self::Node(Node {
-            weight: left.get_weight(),
+            weight: left.len(),
             left: Some(Box::new(left)),
             right: Some(Box::new(right)),
         })
@@ -136,8 +130,8 @@ impl RopeInner {
             Self::Leaf(leaf) => {
                 assert!(i <= leaf.string.len());
                 (
-                    Self::Leaf(Leaf::new(leaf.string[..i].to_string())),
-                    Self::Leaf(Leaf::new(leaf.string[i..].to_string())),
+                    Self::Leaf(Leaf::from(leaf.string[..i].to_string())),
+                    Self::Leaf(Leaf::from(leaf.string[i..].to_string())),
                 )
             }
             Self::Node(node) => {
@@ -163,13 +157,36 @@ impl RopeInner {
             }
         }
     }
+    fn index(&self, index: usize) -> String {
+        match self {
+            RopeInner::Leaf(leaf) => leaf.string[index..index + 1].to_string(),
+            RopeInner::Node(node) => {
+                if index < node.weight {
+                    node.left
+                        .as_ref()
+                        .and_then(|r| Some(r.index(index)))
+                        .unwrap_or_default()
+                } else {
+                    node.right
+                        .as_ref()
+                        .and_then(|r| Some(r.index(index - node.weight)))
+                        .unwrap_or_default()
+                }
+            }
+        }
+    }
+    fn set_string(&mut self, string: String) {
+        if let Self::Leaf(leaf) = self {
+            leaf.string = string;
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Node {
+    weight: usize,
     left: Option<Box<RopeInner>>,
     right: Option<Box<RopeInner>>,
-    weight: usize,
 }
 
 #[derive(Debug)]
@@ -177,9 +194,8 @@ struct Leaf {
     string: String,
     weight: usize,
 }
-
-impl Leaf {
-    fn new(string: String) -> Self {
+impl From<String> for Leaf {
+    fn from(string: String) -> Self {
         Self {
             weight: string.len(),
             string,
@@ -190,24 +206,34 @@ impl Leaf {
 #[cfg(test)]
 mod tests {
     use super::*;
+    fn setup_rope() -> (Rope, String) {
+        let r1 = Rope::from("Hello ");
+        let r2 = Rope::from("my ");
+        let r3 = Rope::from("na");
+        let r4 = Rope::from("me i");
+        let r5 = Rope::from("s");
+        let r6 = Rope::from(" Simon");
+
+        let r_1 = Rope::concat(r1, r2);
+        let r_2 = Rope::concat(r3, r4);
+        let r_3 = Rope::concat(r5, r6);
+        let r = Rope::concat(r_2, r_3);
+        (Rope::concat(r_1, r), "Hello my name is Simon".to_string())
+    }
 
     #[test]
     fn rope_leaf_split_test() {
         let r = Rope::from("Hello, World!");
         let (left, right) = r.split(5);
-        assert_eq!(left.get_string(), "Hello".to_string());
-        assert_eq!(right.get_string(), ", World!".to_string());
+        assert_eq!(left.get_string(), "Hello");
+        assert_eq!(right.get_string(), ", World!");
     }
     #[test]
-    fn rope_concat_test() {
-        let r1 = Rope::from("Hello");
-        let r2 = Rope::from(", ");
-        let r3 = Rope::from("World");
-        let r4 = Rope::from("!");
-        let mut r = Rope::concat(r2, r3);
-        r = Rope::concat(r, r4);
-        r = Rope::concat(r1, r);
-        assert_eq!(r.get_string(), "Hello, World!".to_string())
+    fn rope_concat_and_return_test() {
+        let (r, s) = setup_rope();
+
+        assert_eq!(r.len(), 22);
+        assert_eq!(r.get_string(), s)
     }
     #[test]
     fn rope_node_split_test() {
@@ -232,5 +258,11 @@ mod tests {
         let mut r = Rope::from("Hello, World!");
         r.delete(5, 12);
         assert_eq!(r.get_string(), "Hello!".to_string());
+    }
+    #[test]
+    fn rope_index_test() {
+        let (r, s) = setup_rope();
+        let clsr = |i: usize| assert_eq!(r.index(i), s[i..(i + 1)].to_string());
+        (0..s.len()).for_each(clsr);
     }
 }
